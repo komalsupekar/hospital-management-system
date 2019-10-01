02b9d54f70f80e575ac5854a37ce551e811ce120
madhusudan babar <madhusudanbabar@gmail.com>
2019-10-01 21:10:10 +0530

    improved ui

---
+++ app/src/main/java/com/hospital/komal/Patient/Appointment.java
@@ -0,0 +1,54 @@
+package com.hospital.komal.Patient;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.hospital.komal.Doctor.Specialization;
+import com.hospital.komal.Personal_Info;
+import com.hospital.komal.R;
+
+/**
+ * Created by Neeraj on 07-Apr-16.
+ */
+public class Appointment extends AppCompatActivity {
+
+    String username,password,user_type;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.appointment);
+
+        Bundle bb = getIntent().getExtras();
+        username = bb.getString("username");
+        password = bb.getString("password");
+        user_type = bb.getString("user_type");
+    }
+
+    public void onClick(View view){
+
+        Intent i;
+        Bundle b = new Bundle();
+        b.putString("username",username);
+        b.putString("password",password);
+        b.putString("user_type",user_type);
+
+        switch (view.getId())
+        {
+            case R.id.b_new:
+                i = new Intent(Appointment.this, New_Appointment.class);
+                break;
+           /* case R.id.b_wait:
+                i = new Intent(Appointment.this, Wait_Appointment.class);
+                break;*/
+            default:
+                i = new Intent(Appointment.this, Confirmed_Appointment.class);
+                break;
+        }
+        i.putExtras(b);
+        startActivity(i);
+    }
+
+}
